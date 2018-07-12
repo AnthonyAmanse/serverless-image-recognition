539cb784f014f199cdd5014b5aa0eb850674d7f6
AnthonyAmanse <ghieamanse@gmail.com>
2018-07-12 11:42:23 -0700

    Initial electron code
    This commit adds initial electron code. `npm start` to run the app/
    
    Signed-off-by: AnthonyAmanse <ghieamanse@gmail.com>

--- web/scripts/loadImages.js
+++ web/scripts/loadImages.js
@@ -1,3 +1,5 @@
+var rateLimit = 0;
+
 function getAllImageDocuments() {
   $.ajax({
       url: cloudantURL.origin + "/" + imageDatabase + "/_all_docs",
@@ -6,8 +8,8 @@ function getAllImageDocuments() {
         "Authorization": "Basic " + btoa(cloudantURL.username + ":" + cloudantURL.password)
       },
       success: function (data) {
-        for (var id in data.rows) {
-          getImage(data.rows[id].id)
+        for (var index in data.rows) {
+          getImage(data.rows[index].id)
         }
       },
       error: function (jqXHR, textStatus, errorThrown) {
@@ -23,8 +25,8 @@ function getImage(id) {
 
   var imageSection = document.createElement('div');
   var imageHolder = document.createElement('div');
-  image.src = cloudantURL.href + imageDatabase + "/" + id + "/image"
   image.className = "uploadedImage";
+  loadAttachment(id,image);
   imageSection.id = id
   imageSection.className = "imageSection";
   imageHolder.className = "imageHolder";
@@ -32,6 +34,41 @@ function getImage(id) {
   imageSection.appendChild(imageHolder);
   uploadedImages.prepend(imageSection);
   getDocumentWithId(id, imageSection, 0);
+}
+
+function loadAttachment(id, dom) {
+  if (rateLimit >= 5) {
+    setTimeout(function() {
+      loadAttachment(id,dom);
+    }, 1000);
+  } else {
+    rateLimit++;
+
+    console.log(rateLimit);
+    $.ajax({
+        url: cloudantURL.origin + "/" + imageDatabase + "/" + id + "/image",
+        type: "GET",
+        headers: {
+          "Authorization": "Basic " + btoa(cloudantURL.username + ":" + cloudantURL.password)
+        },
+        xhr:function(){
+          var xhr = new XMLHttpRequest();
+          xhr.responseType= 'blob'
+          return xhr;
+        },
+        success: function (data) {
+          let url = window.URL || window.webkitURL;
+          dom.src = url.createObjectURL(data);
+          rateLimit--;
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          console.log(errorThrown);
+          console.log(textStatus);
+          console.log(jqXHR);
+          rateLimit--;
+        }
+    });
+  }
 }
 
 
